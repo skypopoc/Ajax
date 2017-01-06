f587315850480b5de50f7fed2d1fbc655e694bf7
skypopoc <skypopoc@163.com>
2017-01-06 18:10:59 +0800

    new

---
+++ src/getMap.java
@@ -0,0 +1,34 @@
+import net.sf.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by cvzvb on 2017/1/6.
+ */
+public class getMap extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name=req.getParameter("name");
+        System.out.println(name);
+        Map a=new HashMap();
+        a.put(1,"ss");
+        a.put(2,"ee");
+        req.setAttribute("map", a);
+
+
+    }
+}
---
+++ web/getmap.jsp
@@ -0,0 +1,21 @@
+<%@ page import="java.util.HashMap" %>
+<%@ page import="java.util.Map" %><%--
+  Created by IntelliJ IDEA.
+  User: cvzvb
+  Date: 2017/1/6
+  Time: 17:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form method="post" action="getMap">
+    <input type="text" name="name"/>123
+    <input type="submit"/>
+</form>
+
+</body>
+</html>
